087af4c52fe49b07d06e3340927d19c2d2edb995
vtears <772326244@qq.com>
2019-03-22 16:33:12 +0800

    解决服务端渲染导致router重定义问题

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
     framework: 'app/web/framework',
     store: 'app/web/store',
   },
-  dll: ['vue', 'axios'], // webpack dll 构建
+  dll: ['vue', 'axios', 'vue-router', 'vuex', 'vuex-router-sync'], // webpack dll 构建
   loaders: {},
   plugins: {},
   done() { // 编译完成回调
