6788abd9c9a6082d33fa283b8ffca5d30f3489fb
vtears <772326244@qq.com>
2019-03-22 15:16:43 +0800

    配置单页面的webpack配置

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+'use strict';
+module.exports = {
+  egg: true,
+  framework: 'vue', // 使用 easywebpack-vue 构建解决方案
+  entry: {
+    app: 'app/web/page/app/main.js', // 入口文件
+  },
+  alias: {
+    '~': __dirname,
+    component: 'app/web/component',
+    framework: 'app/web/framework',
+    store: 'app/web/store',
+  },
+  dll: [ 'vue', 'axios' ], // webpack dll 构建
+  loaders: {},
+  plugins: {},
+  done() { // 编译完成回调
+
+  },
+};
