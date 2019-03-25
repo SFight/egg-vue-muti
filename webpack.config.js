079bd8b9198a423763958c19ff6d4c9b6d9020bd
vtears <772326244@qq.com>
2019-03-25 10:49:29 +0800

    增加多界面入口webpack配置

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+export default {
+  egg: true,
+  framework: 'vue',
+  entry: {
+    include: ['app/web/page'],
+    exclude: ['app/web/page/[a-z]+/component', 'app/web/page/test'],
+    loader: {
+      client: 'app/web/framework/vue/entry/client-loader.js',
+      server: 'app/web/framework/vue/entry/server-loader.js',
+    },
+  },
+  alias: {
+    server: 'app/web/framework/vue/entry/server.js',
+    client: 'app/web/framework/vue/entry/client.js',
+    asset: 'app/web/asset',
+    component: 'app/web/component',
+    framework: 'app/web/framework',
+  },
+  dll: ['vue', 'axios'],
+  loaders: {},
+  plugins: {},
+  done() {
+
+  },
+};
